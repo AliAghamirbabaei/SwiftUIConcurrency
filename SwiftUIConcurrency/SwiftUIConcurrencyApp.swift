649de9f29f11e1dbc46c585d4dd54fc652ccd6be
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-02-26 20:57:19 +0330

    Initial Commit

---
+++ SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by Ali Aghamirbabaei on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
