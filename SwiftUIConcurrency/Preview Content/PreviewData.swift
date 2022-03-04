5850d54015f5c49d930bd198489462700517bd4a
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-03-04 15:17:32 +0330

    Added preview data

---
+++ SwiftUIConcurrency/Preview Content/PreviewData.swift
@@ -0,0 +1,10 @@
+//
+//  PreviewData.swift
+//  SwiftUIConcurrency
+//
+//  Created by Ali Aghamirbabaei on 3/4/22.
+//
+
+import Foundation
+
+var coursePreviewData: Course = load("CourseData.json")
