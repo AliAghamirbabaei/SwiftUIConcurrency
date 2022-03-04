eeef6f9659559a0ea2eb730a27139de7543266d8
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-03-04 19:58:27 +0330

    Create view model for section

---
+++ SwiftUIConcurrency/Model/SectionModel.swift
@@ -0,0 +1,28 @@
+//
+//  SectionModel.swift
+//  SwiftUIConcurrency
+//
+//  Created by Ali Aghamirbabaei on 3/4/22.
+//
+
+import Foundation
+
+typealias SectionCollectionData = SectionQuery.Data.SectionCollection
+
+struct SectionDataColletion: Decodable {
+    let sections: [SectionModel]
+    
+    init(_ sectionColletion: SectionCollectionData?) {
+        self.sections = sectionColletion?.sections.map({ section -> SectionModel in
+            SectionModel(section)
+        }) ?? []
+    }
+    
+    struct SectionModel: Decodable {
+        let title: String
+        
+        init(_ section: SectionQuery.Data.SectionCollection.Section?) {
+            self.title = section?.title ?? ""
+        }
+    }
+}
---
+++ SwiftUIConcurrency/ViewModels/SectionViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  SectionViewModel.swift
+//  SwiftUIConcurrency
+//
+//  Created by Ali Aghamirbabaei on 3/4/22.
+//
+
+import Foundation
+import Apollo
+
+class SectionViewModel: ObservableObject {
+    @Published public private(set) var sections: [SectionDataColletion.SectionModel] = []
+    private func querySections() async throws -> GraphQLResult<SectionQuery.Data>? {
+        return await withCheckedContinuation { countinuation in
+            Network.shared.apollo.fetch(query: SectionQuery()) { result in
+                switch result {
+                case .success(let graphQLResult):
+                    countinuation.resume(returning: graphQLResult)
+                case .failure(let error):
+                    if let error = error as? Never {
+                        countinuation.resume(throwing: error)
+                    }
+                }
+            }
+        }
+    }
+    
+    func fetch() async {
+        do {
+            let result = try await querySections()
+            if let result = result {
+                if let sectionCollection = result.data?.sectionCollection {
+                    self.sections = process(data: sectionCollection)
+                }
+            }
+        } catch {
+            print("Error \(error)")
+        }
+    }
+    
+    private func process(data: SectionCollectionData) -> [SectionDataColletion.SectionModel] {
+        let content = SectionDataColletion.init(data)
+        
+        return content.sections
+    }
+}
